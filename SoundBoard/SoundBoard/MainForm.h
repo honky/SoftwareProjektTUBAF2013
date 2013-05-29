90e2063eca64aec7256b1dcc748a8ae4276feda2
Hairy Plotter <plotterhairy@googlemail.com>
2013-05-29 12:22:03 +0200

    correct Trackbar Values

--- SoundBoard/SoundBoard/MainForm.h
+++ SoundBoard/SoundBoard/MainForm.h
@@ -48,8 +48,22 @@ namespace SoundBoard {
 	private: System::Windows::Forms::FlowLayoutPanel^  flowLayoutPanelMasterVolumeControl;
 	private: System::Windows::Forms::GroupBox^  groupBoxMasterVolumeAll;
 	private: System::Windows::Forms::TrackBar^  trackBarMasterVolumeAll;
-	private: System::Windows::Forms::GroupBox^  groupBox1;
-	private: System::Windows::Forms::TrackBar^  trackBar1;
+	private: System::Windows::Forms::GroupBox^  groupBoxMasterVolumeLeft;
+	private: System::Windows::Forms::TrackBar^  trackBarMasterVolumeLeft;
+	private: System::Windows::Forms::GroupBox^  groupBoxMasterVolumeRight;
+	private: System::Windows::Forms::TrackBar^  trackBarMasterVolumeRight;
+
+	private: System::Windows::Forms::GroupBox^  groupBoxMasterVolumeTreble;
+	private: System::Windows::Forms::TrackBar^  trackBarMasterVolumeTreble;
+
+	private: System::Windows::Forms::GroupBox^  groupBoxMasterVolumeBass;
+	private: System::Windows::Forms::TrackBar^  trackBarMasterVolumeBass;
+
+	private: System::Windows::Forms::GroupBox^  groupBoxMasterVolumeBalance;
+	private: System::Windows::Forms::TrackBar^  trackBarMasterVolumeBalance;
+
+
+
 
 
 
@@ -84,15 +98,31 @@ namespace SoundBoard {
 			this->flowLayoutPanelMasterVolumeControl = (gcnew System::Windows::Forms::FlowLayoutPanel());
 			this->groupBoxMasterVolumeAll = (gcnew System::Windows::Forms::GroupBox());
 			this->trackBarMasterVolumeAll = (gcnew System::Windows::Forms::TrackBar());
-			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
-			this->trackBar1 = (gcnew System::Windows::Forms::TrackBar());
+			this->groupBoxMasterVolumeLeft = (gcnew System::Windows::Forms::GroupBox());
+			this->trackBarMasterVolumeLeft = (gcnew System::Windows::Forms::TrackBar());
+			this->groupBoxMasterVolumeRight = (gcnew System::Windows::Forms::GroupBox());
+			this->trackBarMasterVolumeRight = (gcnew System::Windows::Forms::TrackBar());
+			this->groupBoxMasterVolumeTreble = (gcnew System::Windows::Forms::GroupBox());
+			this->trackBarMasterVolumeTreble = (gcnew System::Windows::Forms::TrackBar());
+			this->groupBoxMasterVolumeBass = (gcnew System::Windows::Forms::GroupBox());
+			this->trackBarMasterVolumeBass = (gcnew System::Windows::Forms::TrackBar());
+			this->groupBoxMasterVolumeBalance = (gcnew System::Windows::Forms::GroupBox());
+			this->trackBarMasterVolumeBalance = (gcnew System::Windows::Forms::TrackBar());
 			this->tableLayoutPanelMain->SuspendLayout();
 			this->groupBoxMasterVolumeControl->SuspendLayout();
 			this->flowLayoutPanelMasterVolumeControl->SuspendLayout();
 			this->groupBoxMasterVolumeAll->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeAll))->BeginInit();
-			this->groupBox1->SuspendLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBar1))->BeginInit();
+			this->groupBoxMasterVolumeLeft->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeLeft))->BeginInit();
+			this->groupBoxMasterVolumeRight->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeRight))->BeginInit();
+			this->groupBoxMasterVolumeTreble->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeTreble))->BeginInit();
+			this->groupBoxMasterVolumeBass->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeBass))->BeginInit();
+			this->groupBoxMasterVolumeBalance->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeBalance))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// tableLayoutPanelMain
@@ -119,10 +149,11 @@ namespace SoundBoard {
 			// 
 			this->groupBoxMasterVolumeControl->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left) 
 				| System::Windows::Forms::AnchorStyles::Right));
+			this->groupBoxMasterVolumeControl->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
 			this->groupBoxMasterVolumeControl->Controls->Add(this->flowLayoutPanelMasterVolumeControl);
 			this->groupBoxMasterVolumeControl->Location = System::Drawing::Point(3, 3);
 			this->groupBoxMasterVolumeControl->Name = L"groupBoxMasterVolumeControl";
-			this->groupBoxMasterVolumeControl->Size = System::Drawing::Size(391, 337);
+			this->groupBoxMasterVolumeControl->Size = System::Drawing::Size(391, 161);
 			this->groupBoxMasterVolumeControl->TabIndex = 0;
 			this->groupBoxMasterVolumeControl->TabStop = false;
 			this->groupBoxMasterVolumeControl->Text = L"Master Volume Control";
@@ -132,11 +163,16 @@ namespace SoundBoard {
 			this->flowLayoutPanelMasterVolumeControl->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
 				| System::Windows::Forms::AnchorStyles::Left) 
 				| System::Windows::Forms::AnchorStyles::Right));
+			this->flowLayoutPanelMasterVolumeControl->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
 			this->flowLayoutPanelMasterVolumeControl->Controls->Add(this->groupBoxMasterVolumeAll);
-			this->flowLayoutPanelMasterVolumeControl->Controls->Add(this->groupBox1);
+			this->flowLayoutPanelMasterVolumeControl->Controls->Add(this->groupBoxMasterVolumeLeft);
+			this->flowLayoutPanelMasterVolumeControl->Controls->Add(this->groupBoxMasterVolumeRight);
+			this->flowLayoutPanelMasterVolumeControl->Controls->Add(this->groupBoxMasterVolumeTreble);
+			this->flowLayoutPanelMasterVolumeControl->Controls->Add(this->groupBoxMasterVolumeBass);
+			this->flowLayoutPanelMasterVolumeControl->Controls->Add(this->groupBoxMasterVolumeBalance);
 			this->flowLayoutPanelMasterVolumeControl->Location = System::Drawing::Point(6, 19);
 			this->flowLayoutPanelMasterVolumeControl->Name = L"flowLayoutPanelMasterVolumeControl";
-			this->flowLayoutPanelMasterVolumeControl->Size = System::Drawing::Size(379, 312);
+			this->flowLayoutPanelMasterVolumeControl->Size = System::Drawing::Size(379, 136);
 			this->flowLayoutPanelMasterVolumeControl->TabIndex = 0;
 			// 
 			// groupBoxMasterVolumeAll
@@ -144,7 +180,7 @@ namespace SoundBoard {
 			this->groupBoxMasterVolumeAll->Controls->Add(this->trackBarMasterVolumeAll);
 			this->groupBoxMasterVolumeAll->Location = System::Drawing::Point(3, 3);
 			this->groupBoxMasterVolumeAll->Name = L"groupBoxMasterVolumeAll";
-			this->groupBoxMasterVolumeAll->Size = System::Drawing::Size(56, 122);
+			this->groupBoxMasterVolumeAll->Size = System::Drawing::Size(55, 125);
 			this->groupBoxMasterVolumeAll->TabIndex = 0;
 			this->groupBoxMasterVolumeAll->TabStop = false;
 			this->groupBoxMasterVolumeAll->Text = L"Master";
@@ -154,34 +190,143 @@ namespace SoundBoard {
 			this->trackBarMasterVolumeAll->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
 				| System::Windows::Forms::AnchorStyles::Left) 
 				| System::Windows::Forms::AnchorStyles::Right));
+			this->trackBarMasterVolumeAll->LargeChange = 250;
 			this->trackBarMasterVolumeAll->Location = System::Drawing::Point(6, 19);
+			this->trackBarMasterVolumeAll->Maximum = 1000;
 			this->trackBarMasterVolumeAll->Name = L"trackBarMasterVolumeAll";
 			this->trackBarMasterVolumeAll->Orientation = System::Windows::Forms::Orientation::Vertical;
-			this->trackBarMasterVolumeAll->Size = System::Drawing::Size(41, 97);
+			this->trackBarMasterVolumeAll->Size = System::Drawing::Size(40, 100);
 			this->trackBarMasterVolumeAll->TabIndex = 0;
+			this->trackBarMasterVolumeAll->TickFrequency = 100;
 			this->trackBarMasterVolumeAll->TickStyle = System::Windows::Forms::TickStyle::Both;
+			this->trackBarMasterVolumeAll->Value = 500;
 			// 
-			// groupBox1
+			// groupBoxMasterVolumeLeft
 			// 
-			this->groupBox1->Controls->Add(this->trackBar1);
-			this->groupBox1->Location = System::Drawing::Point(65, 3);
-			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(56, 122);
-			this->groupBox1->TabIndex = 1;
-			this->groupBox1->TabStop = false;
-			this->groupBox1->Text = L"Master";
+			this->groupBoxMasterVolumeLeft->Controls->Add(this->trackBarMasterVolumeLeft);
+			this->groupBoxMasterVolumeLeft->Location = System::Drawing::Point(64, 3);
+			this->groupBoxMasterVolumeLeft->Name = L"groupBoxMasterVolumeLeft";
+			this->groupBoxMasterVolumeLeft->Size = System::Drawing::Size(55, 125);
+			this->groupBoxMasterVolumeLeft->TabIndex = 1;
+			this->groupBoxMasterVolumeLeft->TabStop = false;
+			this->groupBoxMasterVolumeLeft->Text = L"Left";
 			// 
-			// trackBar1
+			// trackBarMasterVolumeLeft
 			// 
-			this->trackBar1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
+			this->trackBarMasterVolumeLeft->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
 				| System::Windows::Forms::AnchorStyles::Left) 
 				| System::Windows::Forms::AnchorStyles::Right));
-			this->trackBar1->Location = System::Drawing::Point(6, 19);
-			this->trackBar1->Name = L"trackBar1";
-			this->trackBar1->Orientation = System::Windows::Forms::Orientation::Vertical;
-			this->trackBar1->Size = System::Drawing::Size(45, 97);
-			this->trackBar1->TabIndex = 0;
-			this->trackBar1->TickStyle = System::Windows::Forms::TickStyle::Both;
+			this->trackBarMasterVolumeLeft->LargeChange = 250;
+			this->trackBarMasterVolumeLeft->Location = System::Drawing::Point(6, 19);
+			this->trackBarMasterVolumeLeft->Maximum = 1000;
+			this->trackBarMasterVolumeLeft->Name = L"trackBarMasterVolumeLeft";
+			this->trackBarMasterVolumeLeft->Orientation = System::Windows::Forms::Orientation::Vertical;
+			this->trackBarMasterVolumeLeft->Size = System::Drawing::Size(45, 100);
+			this->trackBarMasterVolumeLeft->TabIndex = 0;
+			this->trackBarMasterVolumeLeft->TickFrequency = 100;
+			this->trackBarMasterVolumeLeft->TickStyle = System::Windows::Forms::TickStyle::Both;
+			this->trackBarMasterVolumeLeft->Value = 500;
+			// 
+			// groupBoxMasterVolumeRight
+			// 
+			this->groupBoxMasterVolumeRight->Controls->Add(this->trackBarMasterVolumeRight);
+			this->groupBoxMasterVolumeRight->Location = System::Drawing::Point(125, 3);
+			this->groupBoxMasterVolumeRight->Name = L"groupBoxMasterVolumeRight";
+			this->groupBoxMasterVolumeRight->Size = System::Drawing::Size(55, 125);
+			this->groupBoxMasterVolumeRight->TabIndex = 2;
+			this->groupBoxMasterVolumeRight->TabStop = false;
+			this->groupBoxMasterVolumeRight->Text = L"Right";
+			// 
+			// trackBarMasterVolumeRight
+			// 
+			this->trackBarMasterVolumeRight->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
+				| System::Windows::Forms::AnchorStyles::Left) 
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->trackBarMasterVolumeRight->LargeChange = 250;
+			this->trackBarMasterVolumeRight->Location = System::Drawing::Point(6, 19);
+			this->trackBarMasterVolumeRight->Maximum = 1000;
+			this->trackBarMasterVolumeRight->Name = L"trackBarMasterVolumeRight";
+			this->trackBarMasterVolumeRight->Orientation = System::Windows::Forms::Orientation::Vertical;
+			this->trackBarMasterVolumeRight->Size = System::Drawing::Size(45, 100);
+			this->trackBarMasterVolumeRight->TabIndex = 0;
+			this->trackBarMasterVolumeRight->TickFrequency = 100;
+			this->trackBarMasterVolumeRight->TickStyle = System::Windows::Forms::TickStyle::Both;
+			this->trackBarMasterVolumeRight->Value = 500;
+			// 
+			// groupBoxMasterVolumeTreble
+			// 
+			this->groupBoxMasterVolumeTreble->Controls->Add(this->trackBarMasterVolumeTreble);
+			this->groupBoxMasterVolumeTreble->Location = System::Drawing::Point(186, 3);
+			this->groupBoxMasterVolumeTreble->Name = L"groupBoxMasterVolumeTreble";
+			this->groupBoxMasterVolumeTreble->Size = System::Drawing::Size(55, 125);
+			this->groupBoxMasterVolumeTreble->TabIndex = 3;
+			this->groupBoxMasterVolumeTreble->TabStop = false;
+			this->groupBoxMasterVolumeTreble->Text = L"Treble";
+			// 
+			// trackBarMasterVolumeTreble
+			// 
+			this->trackBarMasterVolumeTreble->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
+				| System::Windows::Forms::AnchorStyles::Left) 
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->trackBarMasterVolumeTreble->Location = System::Drawing::Point(6, 19);
+			this->trackBarMasterVolumeTreble->Maximum = 1000;
+			this->trackBarMasterVolumeTreble->Name = L"trackBarMasterVolumeTreble";
+			this->trackBarMasterVolumeTreble->Orientation = System::Windows::Forms::Orientation::Vertical;
+			this->trackBarMasterVolumeTreble->Size = System::Drawing::Size(45, 100);
+			this->trackBarMasterVolumeTreble->TabIndex = 0;
+			this->trackBarMasterVolumeTreble->TickFrequency = 100;
+			this->trackBarMasterVolumeTreble->TickStyle = System::Windows::Forms::TickStyle::Both;
+			this->trackBarMasterVolumeTreble->Value = 500;
+			// 
+			// groupBoxMasterVolumeBass
+			// 
+			this->groupBoxMasterVolumeBass->Controls->Add(this->trackBarMasterVolumeBass);
+			this->groupBoxMasterVolumeBass->Location = System::Drawing::Point(247, 3);
+			this->groupBoxMasterVolumeBass->Name = L"groupBoxMasterVolumeBass";
+			this->groupBoxMasterVolumeBass->Size = System::Drawing::Size(55, 125);
+			this->groupBoxMasterVolumeBass->TabIndex = 3;
+			this->groupBoxMasterVolumeBass->TabStop = false;
+			this->groupBoxMasterVolumeBass->Text = L"Bass";
+			// 
+			// trackBarMasterVolumeBass
+			// 
+			this->trackBarMasterVolumeBass->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
+				| System::Windows::Forms::AnchorStyles::Left) 
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->trackBarMasterVolumeBass->Location = System::Drawing::Point(6, 19);
+			this->trackBarMasterVolumeBass->Maximum = 1000;
+			this->trackBarMasterVolumeBass->Name = L"trackBarMasterVolumeBass";
+			this->trackBarMasterVolumeBass->Orientation = System::Windows::Forms::Orientation::Vertical;
+			this->trackBarMasterVolumeBass->Size = System::Drawing::Size(45, 100);
+			this->trackBarMasterVolumeBass->TabIndex = 0;
+			this->trackBarMasterVolumeBass->TickFrequency = 100;
+			this->trackBarMasterVolumeBass->TickStyle = System::Windows::Forms::TickStyle::Both;
+			this->trackBarMasterVolumeBass->Value = 500;
+			// 
+			// groupBoxMasterVolumeBalance
+			// 
+			this->groupBoxMasterVolumeBalance->Controls->Add(this->trackBarMasterVolumeBalance);
+			this->groupBoxMasterVolumeBalance->Location = System::Drawing::Point(308, 3);
+			this->groupBoxMasterVolumeBalance->Name = L"groupBoxMasterVolumeBalance";
+			this->groupBoxMasterVolumeBalance->Size = System::Drawing::Size(60, 125);
+			this->groupBoxMasterVolumeBalance->TabIndex = 3;
+			this->groupBoxMasterVolumeBalance->TabStop = false;
+			this->groupBoxMasterVolumeBalance->Text = L"Balance";
+			// 
+			// trackBarMasterVolumeBalance
+			// 
+			this->trackBarMasterVolumeBalance->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
+				| System::Windows::Forms::AnchorStyles::Left) 
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->trackBarMasterVolumeBalance->Location = System::Drawing::Point(6, 19);
+			this->trackBarMasterVolumeBalance->Maximum = 1000;
+			this->trackBarMasterVolumeBalance->Minimum = -1000;
+			this->trackBarMasterVolumeBalance->Name = L"trackBarMasterVolumeBalance";
+			this->trackBarMasterVolumeBalance->Orientation = System::Windows::Forms::Orientation::Vertical;
+			this->trackBarMasterVolumeBalance->Size = System::Drawing::Size(50, 100);
+			this->trackBarMasterVolumeBalance->TabIndex = 0;
+			this->trackBarMasterVolumeBalance->TickFrequency = 200;
+			this->trackBarMasterVolumeBalance->TickStyle = System::Windows::Forms::TickStyle::Both;
 			// 
 			// MainForm
 			// 
@@ -197,9 +342,21 @@ namespace SoundBoard {
 			this->groupBoxMasterVolumeAll->ResumeLayout(false);
 			this->groupBoxMasterVolumeAll->PerformLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeAll))->EndInit();
-			this->groupBox1->ResumeLayout(false);
-			this->groupBox1->PerformLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBar1))->EndInit();
+			this->groupBoxMasterVolumeLeft->ResumeLayout(false);
+			this->groupBoxMasterVolumeLeft->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeLeft))->EndInit();
+			this->groupBoxMasterVolumeRight->ResumeLayout(false);
+			this->groupBoxMasterVolumeRight->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeRight))->EndInit();
+			this->groupBoxMasterVolumeTreble->ResumeLayout(false);
+			this->groupBoxMasterVolumeTreble->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeTreble))->EndInit();
+			this->groupBoxMasterVolumeBass->ResumeLayout(false);
+			this->groupBoxMasterVolumeBass->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeBass))->EndInit();
+			this->groupBoxMasterVolumeBalance->ResumeLayout(false);
+			this->groupBoxMasterVolumeBalance->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->trackBarMasterVolumeBalance))->EndInit();
 			this->ResumeLayout(false);
 
 		}
